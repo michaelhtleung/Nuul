735ac7254641d1ba43895154eadb59f997234ab8
Michael Leung <michaelhtleung2@gmail.com>
2019-09-14 17:27:52 -0400

    center streaks Please enter the commit message for your changes. Lines starting

--- src/components/streak.js
+++ src/components/streak.js
@@ -6,9 +6,10 @@ class Streak extends React.Component {
 	render() {
 		return <div className="streak">
 			<h2>{this.props.streakType} Streak:</h2>
-			<h3 className="time">{this.props.time.days} days
+			<h3>{this.props.time.days} days
 				<i className={emojiClass + this.props.emoji} title={":" + this.props.emoji + ":"}></i>
 			</h3>
+			<br/>
 		</div>
 	}
 }
