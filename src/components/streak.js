e42e6b36d486877b7ab774fca1d3b8e719a91dd2
Michael Leung <michaelhtleung2@gmail.com>
2019-09-14 17:24:09 -0400

    center streaks

--- src/components/streak.js
+++ src/components/streak.js
@@ -6,8 +6,9 @@ class Streak extends React.Component {
 	render() {
 		return <div className="streak">
 			<h2>{this.props.streakType} Streak:</h2>
-			<h3>{this.props.time.days} days</h3>
-			<i className={emojiClass + this.props.emoji} title={":" + this.props.emoji + ":"}></i>
+			<h3 className="time">{this.props.time.days} days
+				<i className={emojiClass + this.props.emoji} title={":" + this.props.emoji + ":"}></i>
+			</h3>
 		</div>
 	}
 }
