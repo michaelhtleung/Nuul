9442e41b0bdddde977a6c7c62d3c014c0a546ef2
Michael Leung <michaelhtleung2@gmail.com>
2019-09-14 14:33:48 -0400

    add react-router getting started

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
 import './index.css';
 import App from './App';
+
+import Users from './users'
+import Contact from './contact'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+	<Router>
+		<div>
+			<Route path="/" component={App} />
+			<Route path="/users" component={Users} />
+			<Route path="/contact" component={Contact} />
+		</div>
+	</Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
