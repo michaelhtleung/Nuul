34d2b93703b923add77c46c0026d1e4bc27f4f4c
Michael Leung <michaelhtleung2@gmail.com>
2019-09-14 15:32:47 -0400

    make footer icon reusable

---
+++ src/components/footer-icon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class FooterIcon extends React.Component {
+	render() {
+		return <img src={this.props.filePath} alt="footer image"/>
+	}
+}
+export default FooterIcon;
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import FooterIcon from './footer-icon.js';
+
 import home from '../img/home.svg';
 import achievements from '../img/achievements.svg';
 import account from '../img/account.svg';
 
+
 class Footer extends React.Component {
 	render() {
-		return <div>
-			<img src={home} alt="home"/>
-			<img src={achievements} alt="achievements"/>
-			<img src={account} alt="account"/>
-		</div>
+		return <footer className="footer">
+			<FooterIcon filePath={home}/>
+			<FooterIcon filePath={achievements}/>
+			<FooterIcon filePath={account}/>
+		</footer>
 	}
 }
 export default Footer
