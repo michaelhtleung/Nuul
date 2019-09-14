b34f2d89ee05d724e5d26dcfb23c29c582e44ac9
Michael Leung <michaelhtleung2@gmail.com>
2019-09-14 18:14:20 -0400

    add inspirational quote

--- src/components/motivation.js
+++ src/components/motivation.js
@@ -3,6 +3,8 @@ class Motivation extends React.Component {
 	render() {
 		return <div className="page">
 			<h1>Motivation</h1>
+			<p>Take it one day at a time!</p>
+			<h1>Resources</h1>
 		</div>
 	}
 }
