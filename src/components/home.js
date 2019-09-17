cea6e3616430d7b55df8ed29e24d53e2dabea5e7
Michael Leung <michaelhtleung2@gmail.com>
2019-09-16 20:27:16 -0400

    add bar chart and title with wrong styles and label overlap

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Streak from './streak';
-import {VictoryChart, VictoryBar, VictoryAxis} from 'victory';
+import {VictoryChart, VictoryBar, VictoryAxis, VictoryTheme, VictoryLabel} from 'victory';
 
 var time = {
 	current:{
@@ -17,8 +17,8 @@ var weekday = ["Su", "M", "Tu", "W", "Th", "F", "Sa"];
 
 var aggregateByDays = [
 	{frequency: 10},
-	{frequency: 15},
 	{frequency: 20},
+	{frequency: 15},
 	{frequency: 30},
 	{frequency: 3},
 	{frequency: 50},
@@ -29,19 +29,34 @@ aggregateByDays.forEach( (dayElement, dayIndex) => {
 	dayElement.day = weekday[dayIndex];
 });
 
-console.log(aggregateByDays);
-
 class Home extends React.Component {
 	render() {
 		return <div className="page">
-			<h1>Home</h1>
+			<h1>Nuul</h1>
 			<div className="tile">
-				<Streak streakType={"Current"} time={time.current} emoji="fire"/>
-				<br/>
-				<Streak streakType={"Best"} time={time.best} emoji="open-mouth"/>
+				<h2>Current Streak</h2>
+				<Streak time={time.current} emoji="fire"/>
+				<h2>Best Streak</h2>
+				<Streak time={time.best} emoji="open-mouth"/>
 			</div>
 			<div className="tile">
-				<VictoryBar data={aggregateByDays} x={"day"} y={"frequency"}/>
+				<h2 className="chartTitle">Daily Frequency</h2>
+				<VictoryChart
+					domainPadding={20}
+					theme={VictoryTheme.material}
+					height={300}
+				>
+					<VictoryAxis
+						tickValues={weekday}
+						label={"Day of the Week"}
+					/>
+					<VictoryAxis
+						dependentAxis
+						tickFormat={x => x}
+						label={"Times smoked"}
+					/>
+					<VictoryBar data={aggregateByDays} x={"day"} y={"frequency"}/>
+				</VictoryChart>
 			</div>
 		</div>
 	}
--- src/components/streak.js
+++ src/components/streak.js
@@ -5,7 +5,6 @@ let emojiClass = "twa twa-lg twa-";
 class Streak extends React.Component {
 	render() {
 		return <div className="streak">
-			<h2>{this.props.streakType} Streak:</h2>
 			<h3>{this.props.time.days} days
 				<i className={emojiClass + this.props.emoji} title={":" + this.props.emoji + ":"}></i>
 			</h3>
