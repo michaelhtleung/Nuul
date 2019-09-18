37ff154d53fc20f2d9f8bb5cf1df354b757ea0ef
Michael Leung <michaelhtleung2@gmail.com>
2019-09-17 23:31:23 -0400

    remove circles from footer

--- src/components/footer-icon.js
+++ src/components/footer-icon.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 class FooterIcon extends React.Component {
 	render() {
-		return <div className="iconCircle">
+		return(
 			<img src={this.props.filePath} alt="footer"/>
-		</div>
+	)
 	}
 }
 export default FooterIcon;
