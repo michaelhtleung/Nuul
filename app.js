c90246120c31ef39072585b5ce187cb4145296e8
Michael Leung <michaelhtleung2@gmail.com>
2019-10-04 22:43:17 -0400

    Make requesting non-existant sensors invalid

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-const path = require('path');
+
+const dbFunctions = require("./lib/databaseFunctions.js");
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/expressMongoExample', (err) => {
@@ -11,12 +12,32 @@ mongoose.connect('mongodb://localhost/expressMongoExample', (err) => {
 	console.log("Connected to database")
 });
 
-app.get('/:id/graphData', function (req, res) {
+app.get('/view/graphData/:userid', function (req, res) {
 	res.send('graph');
 });
 
-app.get('/:id/achievementsData', function (req, res) {
+app.get('/view/achievementsData/:userid', function (req, res) {
 	res.send('achievements');
+});
+
+app.get('/view/sensor/:serial', function (req, res) {
+	dbFunctions.getSensorData(req.params.serial, (document) => {
+		if (document) {
+			res.send(document);
+		} {
+			res.status(404).send("requested sensor does not exist");
+		}
+	});
+});
+
+app.post('/add/sensor/:serial', function (req, res) {
+});
+
+app.post('/add/session', function (req, res) {
+});
+
+app.post('/addUser', function (req, res) {
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
