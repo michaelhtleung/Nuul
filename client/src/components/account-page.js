c3ecb405b2d77e83287d125c5317ce136cd76f4f
Michael Leung <michaelhtleung2@gmail.com>
2019-10-06 22:58:43 -0400

    Add material ui radio buttons for user selection

--- client/src/components/account-page.js
+++ client/src/components/account-page.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+
+const reloadUserData = () => {
+	console.log('reload user data');
+};
+
 class Achievements extends React.Component {
+
 	render() {
 		return <div className="page">
-			<h1>Account</h1>
+			<h1>{"Accounts".toUpperCase()}</h1>
+			<FormControl component="fieldset">
+				<FormLabel component="legend"/>
+				<RadioGroup name="userID" onChange={reloadUserData}>
+					<FormControlLabel value="andy" control={<Radio/>} label="Andy"/>
+					<FormControlLabel value="barry" control={<Radio/>} label="Barry"/>
+					<FormControlLabel value="cindy" control={<Radio/>} label="Cindy"/>
+				</RadioGroup>
+			</FormControl>
 		</div>
 	}
 }
--- client/src/components/achievements-page.js
+++ client/src/components/achievements-page.js
@@ -2,7 +2,7 @@ import React from 'react'
 class AchievementsPage extends React.Component {
 	render() {
 		return <div className="page">
-			<h1>Achievements</h1>
+			<h1>{"Achievements".toUpperCase()}</h1>
 		</div>
 	}
 }
