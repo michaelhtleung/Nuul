0a3ff4983b52a9604a3f0d3199cadf5d4ed39f5a
Michael Leung <michaelhtleung2@gmail.com>
2020-02-10 00:53:58 -0500

    Add comment on model performance

--- model.py
+++ model.py
@@ -14,4 +14,5 @@ X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test
 model = DecisionTreeClassifier(random_state=1)
 model.fit(X_train, y_train)
 print("Training Score: ", model.score(X_train, y_train))
+# if this is between 85% and 90%, the model does not underfit or overfit, which is good
 print("Testing Score: ", model.score(X_valid, y_valid))
